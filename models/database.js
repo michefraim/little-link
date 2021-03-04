d0cb56723430a3930e7008deeddfb281f344a4de
michefraim <michefraim@gmail.com>
2021-03-04 16:25:05 +0200

    database methods added

--- models/database.js
+++ models/database.js
@@ -2,24 +2,48 @@
 const fs = require("fs");
 
 module.exports = class DataBase {
-    static async readDataBaseData() {
-       const fileData = await JSON.parse(
-            fs.fsPromises.readFile("../database.json")
-          );
-          return fileData;
-    }
-
-  static async doesUrlAlreadyExists(originUrl) {
-        const fileData = this.readDataBaseData();
-        const filteredData = fileData.filter((LittleLink) => {
-            return LittleLink.originUrl;
-        })
-        return (filteredData.length === 0) ? false : true;
+  static async readDataBaseData() {
+      try {
+    const fileData = await JSON.parse(
+      fs.fsPromises.readFile("../database.json")
+    );
+    return fileData;
+      }
+      catch (e) {
+          return `database read failed ${e}`;
+      }
   }
 
-  static async postNewUrl(newData) {
-        this.readDataBaseData();
+  static async doesUrlAlreadyExists(originUrl) {
+    const fileData = this.readDataBaseData();
+    try {
+        const filteredData = fileData.filter((LittleLink) => {
+          return LittleLink.originUrl === originUrl.toLowerCase();
+        });
+        return filteredData.length === 0 ? false : true;
+    }
+    catch (e) {
+        return `checking for url failed ${e}`;
+    }
+  }
+
+  static async addNewData(newData) {
+      if (this.doesUrlAlreadyExists(newData.originUrl)) {
+        const filteredData = fileData.filter((LittleLink) => {
+            return LittleLink.originUrl === originUrl.toLowerCase();
+          });
+          return filteredData;
+      }
+
+    const fileData = this.readDataBaseData();
     fileData.push(newData);
-    fs.fsPromises.writeFile('sample.json', JSON.stringify(fileData, null, 2));
+    try {
+        await fs.fsPromises.writeFile("sample.json", JSON.stringify(fileData, null, 2));
+        return;
+    }
+    catch (e) {
+        return `Adding data failed ${e}`;
+    }
+
   }
 };
