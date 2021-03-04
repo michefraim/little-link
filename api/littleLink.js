f999dc143f294bd25e1651738feadb80707a07ac
michefraim <michefraim@gmail.com>
2021-03-04 16:49:06 +0200

    get by shortUrl added

--- api/littleLink.js
+++ api/littleLink.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const fs = require("fs");
+const DataBase = require("../models/database");
+const LittleLink = require("../models/littleLink");
 
 const router = express.Router();
 
 router.use(express.json());
 
 router.get("/", (request, response) => {
-    return response.status(404).json({ message: "Error No ID" });
+  return response.status(404).json({ message: "Error No ID" });
 });
 
-router.get("/:id", (request, response) => {
-    const { id } = request.params;
-
-    });
+router.get("/:shortUrl", (request, response) => {
+  const { shortUrl } = request.params;
+  try {
+    const data = DataBase.readDataBaseByShortUrl(shortUrl);
+    if (data === "Not Found") {
+      return response
+        .status(200)
+        .json({ error: "No short URL found for the given input" });
+    }
+  } catch (e) {
+    return response
+      .status(500)
+      .json({ error: `failed reading the database`, fullerror: e });
+  }
+});
 
 module.exports = router;
--- models/database.js
+++ models/database.js
@@ -3,47 +3,63 @@ const fs = require("fs");
 
 module.exports = class DataBase {
   static async readDataBaseData() {
-      try {
-    const fileData = await JSON.parse(
-      fs.fsPromises.readFile("../database.json")
-    );
-    return fileData;
+    try {
+      const fileData = await JSON.parse(
+        fs.fsPromises.readFile("../database.json")
+      );
+      return fileData;
+    } catch (e) {
+      return `database read failed ${e}`;
+    }
+  }
+
+  static async readDataBaseByShortUrl(shortUrl) {
+    const fileData = this.readDataBaseData();
+    try {
+      const filteredData = fileData.filter((LittleLink) => {
+        return LittleLink.shortUrl === shortUrl.toLowerCase();
+      });
+
+      if (filteredData.length === 0) {
+        return "Not Found";
       }
-      catch (e) {
-          return `database read failed ${e}`;
-      }
+      
+      return filteredData;
+    } catch (e) {
+      return `ShortUrl failed ${e}`;
+    }
   }
 
   static async doesUrlAlreadyExists(originUrl) {
     const fileData = this.readDataBaseData();
     try {
-        const filteredData = fileData.filter((LittleLink) => {
-          return LittleLink.originUrl === originUrl.toLowerCase();
-        });
-        return filteredData.length === 0 ? false : true;
-    }
-    catch (e) {
-        return `checking for url failed ${e}`;
+      const filteredData = fileData.filter((LittleLink) => {
+        return LittleLink.originUrl === originUrl.toLowerCase();
+      });
+      return filteredData.length === 0 ? false : true;
+    } catch (e) {
+      return `checking for url failed ${e}`;
     }
   }
 
   static async addNewData(newData) {
-      if (this.doesUrlAlreadyExists(newData.originUrl)) {
-        const filteredData = fileData.filter((LittleLink) => {
-            return LittleLink.originUrl === originUrl.toLowerCase();
-          });
-          return filteredData;
-      }
+    if (this.doesUrlAlreadyExists(newData.originUrl)) {
+      const filteredData = fileData.filter((LittleLink) => {
+        return LittleLink.originUrl === originUrl.toLowerCase();
+      });
+      return filteredData;
+    }
 
     const fileData = this.readDataBaseData();
     fileData.push(newData);
     try {
-        await fs.fsPromises.writeFile("sample.json", JSON.stringify(fileData, null, 2));
-        return;
+      await fs.fsPromises.writeFile(
+        "sample.json",
+        JSON.stringify(fileData, null, 2)
+      );
+      return;
+    } catch (e) {
+      return `Adding data failed ${e}`;
     }
-    catch (e) {
-        return `Adding data failed ${e}`;
-    }
-
   }
 };
